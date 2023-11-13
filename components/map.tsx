797a9f7c4087bc5f1d8ce6c841d6027f4f6ca3d4
Leonardo Villa <villaleonardo99@gmail.com>
2023-11-13 15:30:28 -0500

    map

---
+++ components/map.tsx
@@ -0,0 +1,17 @@
+// Package
+import * as React from "react"
+import { Button } from "@mui/material"
+import { Link } from 'react-router-dom'
+
+// Component
+import TextFields from '@/components/TextFields'
+
+
+export default function Map(){
+    return(
+        <div className="map">
+            
+        </div>
+        
+    )
+}
