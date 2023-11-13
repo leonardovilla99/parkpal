94e2a1d722a83fffdc55fde6e3768a56ac0609ce
Leonardo Villa <villaleonardo99@gmail.com>
2023-11-13 15:21:49 -0500

    map

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,27 +11,28 @@ import './globals.css'
 // Component
 import Login from '@/components/Login'
 import Register from '@/components/Register'
+import Map from '@/components/Map'
 
 // Initialization
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout(){
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
     useEffect(() => {
         document.title = 'ParkPal';
     }, []);
     if(isLoggedIn) {
-        return (<>prova</>)
+        return (<Map></Map>)
     }else{
         return(
         <html lang="en">
-    <body className={inter.className}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/register" element={<Register/>} />
-                </Routes>
-            </BrowserRouter>
+            <body className={inter.className}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/register" element={<Register/>} />
+                    </Routes>
+                </BrowserRouter>
             </body>
         </html>
         )
